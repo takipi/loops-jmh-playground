f263f93f25b7008c6bab60145b9393adfdfc1b2c
Yan Bonnel <ybonnel@gmail.com>
2015-11-25 05:25:09 +0100

    Fix parallelStreamMaxInteger
    Before :
    LoopBenchmarkMain.parallelStreamMaxInteger  avgt   10  0,490 ± 0,075  ms/op
    
    After :
    LoopBenchmarkMain.parallelStreamMaxInteger  avgt   10  0,067 ± 0,004  ms/op

--- src/main/java/com/takipi/oss/benchmarks/jmh/loops/LoopBenchmarkMain.java
+++ src/main/java/com/takipi/oss/benchmarks/jmh/loops/LoopBenchmarkMain.java
@@ -116,8 +116,7 @@ public class LoopBenchmarkMain {
 	@Measurement(iterations = 5)
 	@Warmup(iterations = 5)
 	public int parallelStreamMaxInteger() {
-		Optional<Integer> max = integers.parallelStream().reduce(Integer::max);
-		return max.get();
+		return integers.parallelStream().mapToInt(Integer::intValue).reduce(Integer.MIN_VALUE, Integer::max);
 	}
 
 	@Benchmark
